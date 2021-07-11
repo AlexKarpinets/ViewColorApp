96185261a8ce04552bacdfd35baca7d1b188d88d
AlexKarpinets <66379289+AlexKarpinets@users.noreply.github.com>
2021-07-11 22:01:04 +0300

    Add code

--- ViewColorApp/ViewController.swift
+++ ViewColorApp/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorsWindows: UIView!
+    
+    @IBOutlet weak var redLabelInt: UILabel!
+    @IBOutlet weak var greenLabelInt: UILabel!
+    @IBOutlet weak var blueLabelInt: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorsWindows.layer.cornerRadius = 15
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        redLabelInt.text = String(redSlider.value)
+        self.colorsWindows.backgroundColor = UIColor(red:CGFloat(redSlider.value)/255, green: CGFloat(greenSlider.value)/255, blue: CGFloat(blueSlider.value)/255, alpha: 1)
+    }
+    @IBAction func greenSliderAction() {
+        greenLabelInt.text = String(greenSlider.value)
+        self.colorsWindows.backgroundColor = UIColor(red:CGFloat(redSlider.value)/255, green: CGFloat(greenSlider.value)/255, blue: CGFloat(blueSlider.value)/255, alpha: 1)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabelInt.text = String(blueSlider.value)
+        self.colorsWindows.backgroundColor = UIColor(red:CGFloat(redSlider.value)/255, green: CGFloat(greenSlider.value)/255, blue: CGFloat(blueSlider.value)/255, alpha: 1)
+    }
 }
 
